0ab1ee887c221ae95f5126580685bcfc268fd4dc
Kolio Kolev <114016354+KStKolev@users.noreply.github.com>
2023-11-03 22:55:45 +0200

    Add files via upload
    Generics - Lab

---
+++ E11. Generic/P01. Box/Box.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private int count;
+        private List<T> elements;
+
+        public Box()
+        {
+            elements = new List<T>();
+        }
+
+        public int Count { get { return this.count; } }
+
+        public void Add(T element) 
+        {
+            elements.Add(element);
+            count++;
+        }
+
+        public T Remove() 
+        {
+            count--;
+            T removedElement = elements.Last();
+            elements.RemoveAt(count);
+            return removedElement;
+        }
+    }
+}
---
+++ E11. Generic/P02. Array Creator/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string[] strings = ArrayCreator.Create(5, "Pesho");
+            System.Console.WriteLine(string.Join(" ", strings));
+            int[] integers = ArrayCreator.Create(10, 33);
+            System.Console.WriteLine(string.Join(" ", integers));
+            System.Console.Read();
+        }
+    }
+}

78d12d48ebc7127ce76184abc129752e9149fe90
Kolio Kolev <114016354+KStKolev@users.noreply.github.com>
2023-11-03 23:16:21 +0200

    Add files via upload
    Iterators and Comparators - Lab

---
+++ E13. Iterators and Comparators/P04. BookComparer/BookComparator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    public class BookComparator : IComparer<Book>
+    {
+        public int Compare(Book x, Book y)
+        {
+            int result = x.CompareTo(y);
+            if (result == 0)
+            {
+                result = y.CompareTo(x);
+            }
+            return result;
+        }
+    }
+}
